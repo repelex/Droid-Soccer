72762a5384394cb1efe927f2b39679f7e66f5e84
vallan16 <vallan16@hotmail.com>
2016-01-20 12:27:10 -0500

    Added a Game Menu scene
    Removed the game Menu text UI from the play scene

---
+++ Assets/Scripts/Play game.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Playgame : MonoBehaviour {
+
+	public void PlayGame()
+    {
+        Application.loadedLevel();/*Needs to add a int value from play game however unsure how we want to set player setup in game play
+        atm it showed just switch to game scene from main menu scene*/
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+
+    }
+}
