3eb64fd949806f2c94e7020871189a87b6fed853
repelex <repelex_1@hotmail.com>
2016-01-27 15:52:07 -0500

    Fixed bug relating to deleted files

--- Assets/ColorRPC.cs
+++ Assets/ColorRPC.cs
@@ -6,19 +6,15 @@ public class ColorRPC : MonoBehaviour {
     [PunRPC]
     public void ChangeColorToBlue()
     {
-        GameObject controller = GameObject.Find("Control");
         gameObject.GetComponent<Renderer>().material.color = new Vector4(0, 0, 1, 1);
-        controller.GetComponent<TeamManager>().bluePlayers++;
         
     }
 
     [PunRPC]
     public void ChangeColorToRed()
     {
-        Debug.Log("red team");
-        GameObject controller = GameObject.Find("Control");
         gameObject.GetComponent<Renderer>().material.color = new Vector4(1, 0, 0, 1);
-        controller.GetComponent<TeamManager>().redPlayers++;
+
     }
 
 
