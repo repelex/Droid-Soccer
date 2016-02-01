d41ffbe1485a217323bde5b7cda1c8be66eeee26
repelex <repelex_1@hotmail.com>
2016-02-01 11:54:51 -0500

    Added experimental camera (see desc.)
    Enable by selecting the camera in the scene and ticking the
    "experimental" checkbox in the cameracontroller component. Does not work
    well with current movement.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
+    public GameObject target;
     public GameObject player;
+    public bool experimental = false;
 
     private Vector3 offset;
 
@@ -14,6 +16,21 @@ public class CameraController : MonoBehaviour {
 
     void LateUpdate ()
     {
-        transform.position = player.transform.position + offset;
+        if (!experimental)
+        {
+            transform.position = player.transform.position + offset;
+        }
+        else {
+            
+            transform.position = player.transform.position;
+            Vector3 playerballvec = new Vector3(target.transform.position.x - player.transform.position.x, target.transform.position.y - player.transform.position.y, target.transform.position.z - player.transform.position.z);
+            playerballvec.Normalize();
+            playerballvec.Scale(new Vector3(3, 3, 3));
+            transform.position = new Vector3(player.transform.position.x - playerballvec.x,
+                                            player.transform.position.y - playerballvec.y + 3,
+                                            player.transform.position.z - playerballvec.z);
+            transform.LookAt(target.transform);
+        }
     }
+
 }
