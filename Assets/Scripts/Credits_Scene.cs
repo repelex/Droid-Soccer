760f687445a957d680a3dc3f3dbe9adee77c2faa
vallan16 <vallan16@hotmail.com>
2016-02-29 11:11:39 -0500

    partially done credits scrolling

---
+++ Assets/Scripts/Credits_Scene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits_Scene : MonoBehaviour {
+
+
+    public static Vector2 BeginScrollView(Rect position, Vector2 scrollPosition, Rect viewRect);
+    public static Vector2 BeginScrollView(Rect position, Vector2 scrollPosition, Rect viewRect, bool alwaysShowHorizontal, bool alwaysShowVertical);
+    public static Vector2 BeginScrollView(Rect position, Vector2 scrollPosition, Rect viewRect, GUIStyle horizontalScrollbar, GUIStyle verticalScrollbar);
+    public static Vector2 BeginScrollView(Rect position, Vector2 scrollPosition, Rect viewRect, bool alwaysShowHorizontal, bool alwaysShowVertical, GUIStyle horizontalScrollbar, GUIStyle verticalScrollbar);
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
