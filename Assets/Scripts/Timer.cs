411004ec132858546258c9057e985c530a32cdfd
ajmccutc <mccutcheon.andrew.j@gmail.com>
2016-03-02 10:56:56 -0500

    David's Timer
    Timer Works

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    public float timer = 500;
+    Text txt;
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        txt.text = timer.ToString("00:00");
+        if (timer <= 0)
+        {
+            timer = 0;
+        }
+    }
+
+    void Start()
+    {
+        txt = GetComponent<Text>();
+        txt.text = "0.00";
+    }
+}
+
