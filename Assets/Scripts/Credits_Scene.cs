eef9f7e55cc2cf17d6dd1100c8e42dbaa9ac26c6
vallan16 <vallan16@hotmail.com>
2016-03-02 10:29:15 -0500

    commented out compile errors on credit scene

--- Assets/Scripts/Credits_Scene.cs
+++ Assets/Scripts/Credits_Scene.cs
@@ -4,10 +4,10 @@ using System.Collections;
 public class Credits_Scene : MonoBehaviour {
 
 
-    public static Vector2 BeginScrollView(Rect position, Vector2 scrollPosition, Rect viewRect);
-    public static Vector2 BeginScrollView(Rect position, Vector2 scrollPosition, Rect viewRect, bool alwaysShowHorizontal, bool alwaysShowVertical);
-    public static Vector2 BeginScrollView(Rect position, Vector2 scrollPosition, Rect viewRect, GUIStyle horizontalScrollbar, GUIStyle verticalScrollbar);
-    public static Vector2 BeginScrollView(Rect position, Vector2 scrollPosition, Rect viewRect, bool alwaysShowHorizontal, bool alwaysShowVertical, GUIStyle horizontalScrollbar, GUIStyle verticalScrollbar);
+    //public static Vector2 BeginScrollView(Rect position, Vector2 scrollPosition, Rect viewRect);
+    //public static Vector2 BeginScrollView(Rect position, Vector2 scrollPosition, Rect viewRect, bool alwaysShowHorizontal, bool alwaysShowVertical);
+    //public static Vector2 BeginScrollView(Rect position, Vector2 scrollPosition, Rect viewRect, GUIStyle horizontalScrollbar, GUIStyle verticalScrollbar);
+    //public static Vector2 BeginScrollView(Rect position, Vector2 scrollPosition, Rect viewRect, bool alwaysShowHorizontal, bool alwaysShowVertical, GUIStyle horizontalScrollbar, GUIStyle verticalScrollbar);
     // Use this for initialization
     void Start () {
 	
