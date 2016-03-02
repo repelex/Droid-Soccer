cbb3c2e75d57b4831bf991623f7ebc795bb1d4ad
kmacdon1 <kmacdon1@lakeheadu.ca>
2016-03-02 11:10:01 -0500

    pause menu functional

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject menu;
+    private bool isShowing = false;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("escape"))
+        {
+            isShowing = !isShowing;
+            menu.SetActive(isShowing);
+        }
+    }
+
+    //Quit Game
+    public void QuitGame()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
