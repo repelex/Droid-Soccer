15cd32b2b2150d88381250b3f005b8e414e5943e
vallan16 <vallan16@hotmail.com>
2016-03-04 11:16:05 -0500

    Text now loops around properly

--- Assets/Scripts/Credits_Scene.cs
+++ Assets/Scripts/Credits_Scene.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class Credits_Scene : MonoBehaviour {
     public GameObject credits;
+
     // Use this for initialization
     void Start () {
-        credits.transform.Translate(0, 1,0);
+        Application.targetFrameRate = 20;
     }
 	
 	// Update is called once per frame
 	void Update () {
+        
+        Application.targetFrameRate = 20;
         credits.transform.Translate(0, 1,0);
+       
+        if (credits.transform.position.y >= (Screen.height*2 + 100)){
+            
+            credits.transform.Translate(0, -Screen.height*2 - 100, 0);
+        }
     }
 }
