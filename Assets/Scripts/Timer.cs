efa9618afaf07ae02daffc9e7999c840a9241af3
vallan16 <vallan16@hotmail.com>
2016-03-16 11:17:45 -0400

    Possible fix to timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,28 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
 
     public float timer = 500;
     Text txt;
+    
 
     void Update()
     {
         timer -= Time.deltaTime;
-        txt.text = timer.ToString("00:00");
         
+        txt.text = string.Format("00:00", timer.ToString());
+
         if (timer <= 0)
         {
             timer = 0;
+            SceneManager.LoadScene("Lobby");
         }
     }
 
     void Start()
     {
         txt = GetComponent<Text>();
-        txt.text = "0.00";
+        txt.text = "00.00";
     }
 }
 
