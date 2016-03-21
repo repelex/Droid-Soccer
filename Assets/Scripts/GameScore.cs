b7493fc90a6c6dc413dfcd35a3c32993333d8b8a
vallan16 <vallan16@hotmail.com>
2016-03-21 10:39:33 -0400

    Just added some scripts that don't work yet

---
+++ Assets/Scripts/GameScore.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameScore : MonoBehaviour {
+    public GameObject Player_Scores;
+    private bool isShowing = false;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("Tab")){
+            isShowing = !isShowing;
+            Player_Scores.SetActive(isShowing);
+        }
+    }
+}
