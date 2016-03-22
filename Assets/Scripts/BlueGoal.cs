b999557e624e77b89a246a1ec1a49c829dffa0f5
Cody Antcliffe <codyantcliffe@gmail.com>
2016-03-22 16:06:29 -0400

    Can Detect Goals
    Almost complete,  just need to change timing of position resets and keep
    score throughout the game.

---
+++ Assets/Scripts/BlueGoal.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BlueGoal : MonoBehaviour {
+
+     GameObject RTS;
+     int RScore;
+     void Start()
+     {
+          
+          //Create ref to the object before deactivating
+          RTS = GameObject.Find("RedTeamScored");
+          RTS.SetActive(false);
+     }
+     void incrementScore()
+     {
+          GameObject RScoreOb = GameObject.Find("Red Team Score");
+      
+          int r = int.Parse(RScoreOb.GetComponent<Text>().text);
+          r++;
+          RScoreOb.GetComponent<Text>().text = r.ToString();
+     }
+
+
+     // Update is called once per frame
+     void Update()
+     {
+
+     }
+
+     //Detect when ball entes goal area
+     void OnTriggerExit(Collider obj)
+     {
+
+
+          if (obj.gameObject.name == "Ball")
+          {
+               Debug.Log("Red Team Scored!");
+               incrementScore();
+               RTS.SetActive(true);
+
+               SceneManager.LoadScene("MiniGame");
+          }
+     }
+}
---
+++ Assets/Scripts/RedGoal.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RedGoal : MonoBehaviour {
+
+     GameObject BTS;
+     int BScore;
+     void Start()
+     {
+          //Create ref to the object before deactivating
+          BTS = GameObject.Find("BlueTeamScored");
+          BTS.SetActive(false);
+     }
+
+     bool canScore = true;
+     void incrementScore()
+     {
+          GameObject BScoreOb = GameObject.Find("Blue Team Score");
+          
+          int b = int.Parse(BScoreOb.GetComponent<Text>().text);
+          b++;
+          BScoreOb.GetComponent<Text>().text = b.ToString();
+          canScore = false;
+     }
+
+
+     // Update is called once per frame
+     void Update()
+     {
+          
+     }
+
+     //Detect when ball entes goal area
+     void OnTriggerExit(Collider obj)
+     {
+          if (obj.gameObject.name == "Ball" && canScore)
+          {
+               Debug.Log("Blue Team Scored!");
+               incrementScore();
+               BTS.SetActive(true);
+               
+               SceneManager.LoadScene("MiniGame");
+          }
+     }
+          
+     
+}
