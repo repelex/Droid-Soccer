83993800a3635f433be4935625a545f9a3ee5cb6
kmacdon1 <kmacdon1@lakeheadu.ca>
2016-04-01 11:23:50 -0400

    audio clips for goal

--- Assets/Scripts/BlueGoal.cs
+++ Assets/Scripts/BlueGoal.cs
@@ -3,107 +3,121 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System;
- 
-public class BlueGoal : MonoBehaviour {
 
-     GameObject RTS;
-     int RScore;
-     bool isSynced = false;
-     void Start()
-     {
-         
-          //Create ref to the object before deactivating
-          RTS = GameObject.Find("RedTeamScored");
-          RTS.SetActive(false);
-          getScores();
-     }
-     bool canScore = true;
-     void incrementScore()
-     {
-          GameObject RScoreOb = GameObject.Find("Red Team Score");
+public class BlueGoal : MonoBehaviour
+{
 
-          RScore++;
-          RScoreOb.GetComponent<Text>().text = RScore.ToString();
-          canScore = false;
-          saveScores();
+    GameObject RTS;
+    int RScore;
+    bool isSynced = false;
 
-     }
+    public AudioClip scoreSound;
+    private AudioSource source;
+    private float volLowRange = .5f;
+    private float volHighRange = 1.0f;
 
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
 
-     // Update is called once per frame
-     void Update()
-     {
-          saveTime();
-          if (isTimerOn)
-          {
-               afterGoalTimer();
-          }
-          if (!isSynced)
-          {
-               GameObject.Find("Red Team Score").GetComponent<Text>().text = RScore.ToString();
-               GameObject.Find("Timer").GetComponent<Timer>().timer = (float)PlayerPrefs.GetInt("Time"); 
-               isSynced = true;
+    void Start()
+    {
 
-          }
-     }
+        //Create ref to the object before deactivating
+        RTS = GameObject.Find("RedTeamScored");
+        RTS.SetActive(false);
+        getScores();
+    }
+    bool canScore = true;
+    void incrementScore()
+    {
+        float vol = UnityEngine.Random.Range(volLowRange, volHighRange);
+        source.PlayOneShot(scoreSound, vol);
 
-     //Detect when ball entes goal area
-     void OnTriggerExit(Collider obj)
-     {
-          if (obj.gameObject.name == "Ball")
-          {
-               Debug.Log("Red Team Scored!");
-               incrementScore();
-               RTS.SetActive(true);
-               isTimerOn = true;
-               
-          }
-     }
+        GameObject RScoreOb = GameObject.Find("Red Team Score");
+        RScore++;
+        RScoreOb.GetComponent<Text>().text = RScore.ToString();
+        canScore = false;
+        saveScores();
 
-     //Wait 3 seconds before reloading the scene
-     public float timer = 4;
-     bool isTimerOn = false;
-     void afterGoalTimer()
-     {
-          timer -= Time.deltaTime;
-          if (timer <= 0)
-          {
-               saveTime();
-               saveScores();
-              
-               SceneManager.LoadScene("MiniGame");
-               
-          }
-     }
-
-     //Load scores
-     void getScores()
-     {
-          //Check if we already have the value in playerprefs
-          if (!PlayerPrefs.HasKey("RedScore"))
-          {
-               RScore = 0;
-               PlayerPrefs.SetInt("RedScore", 0);
-          }
-          else
-          {
-               RScore = PlayerPrefs.GetInt("RedScore");
-          }
+    }
 
 
-     }
-     //Used for continuing the scores between goals
-     void saveScores()
-     {
-          PlayerPrefs.SetInt("RedScore", RScore);
-     }
+    // Update is called once per frame
+    void Update()
+    {
+        saveTime();
+        if (isTimerOn)
+        {
+            afterGoalTimer();
+        }
+        if (!isSynced)
+        {
+            GameObject.Find("Red Team Score").GetComponent<Text>().text = RScore.ToString();
+            GameObject.Find("Timer").GetComponent<Timer>().timer = (float)PlayerPrefs.GetInt("Time");
+            isSynced = true;
 
-     //Saves the current time on the clock
-     void saveTime()
-     {
-          string currTime = GameObject.Find("Timer").GetComponent<Text>().text.ToString();
-          string[] times = currTime.Split(':');
-          int timeRemaining = (Int32.Parse(times[0]) * 60) + Int32.Parse(times[1]);
-          PlayerPrefs.SetInt("Time", timeRemaining);
-     }
+        }
+    }
+
+    //Detect when ball entes goal area
+    void OnTriggerExit(Collider obj)
+    {
+        if (obj.gameObject.name == "Ball")
+        {
+            Debug.Log("Red Team Scored!");
+            incrementScore();
+            RTS.SetActive(true);
+            isTimerOn = true;
+
+        }
+    }
+
+    //Wait 3 seconds before reloading the scene
+    public float timer = 4;
+    bool isTimerOn = false;
+    void afterGoalTimer()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            saveTime();
+            saveScores();
+
+            SceneManager.LoadScene("MiniGame");
+
+        }
+    }
+
+    //Load scores
+    void getScores()
+    {
+        //Check if we already have the value in playerprefs
+        if (!PlayerPrefs.HasKey("RedScore"))
+        {
+            RScore = 0;
+            PlayerPrefs.SetInt("RedScore", 0);
+        }
+        else
+        {
+            RScore = PlayerPrefs.GetInt("RedScore");
+        }
+
+
+    }
+    //Used for continuing the scores between goals
+    void saveScores()
+    {
+        PlayerPrefs.SetInt("RedScore", RScore);
+    }
+
+    //Saves the current time on the clock
+    void saveTime()
+    {
+        string currTime = GameObject.Find("Timer").GetComponent<Text>().text.ToString();
+        string[] times = currTime.Split(':');
+        int timeRemaining = (Int32.Parse(times[0]) * 60) + Int32.Parse(times[1]);
+        PlayerPrefs.SetInt("Time", timeRemaining);
+    }
 }
--- Assets/Scripts/RedGoal.cs
+++ Assets/Scripts/RedGoal.cs
@@ -4,103 +4,120 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System;
 
-public class RedGoal : MonoBehaviour {
+public class RedGoal : MonoBehaviour
+{
 
-     GameObject BTS;
-     int BScore;
-     bool isSynced = false;
-     void Start()
-     {
-          //Create ref to the object before deactivating
-          BTS = GameObject.Find("BlueTeamScored");
-          BTS.SetActive(false);
-          getScores();
-         
-     }
+    GameObject BTS;
+    int BScore;
+    bool isSynced = false;
 
-     bool canScore = true;
-     void incrementScore()
-     {
-          GameObject BScoreOb = GameObject.Find("Blue Team Score");
-
-          BScore++;
-          BScoreOb.GetComponent<Text>().text = BScore.ToString();
-          canScore = false;
-          saveScores();
-     }
+    public AudioClip scoreSound;
+    private AudioSource source;
+    private float volLowRange = .5f;
+    private float volHighRange = 1.0f;
 
 
-     // Update is called once per frame
-     void Update()
-     {
-          if (isTimerOn)
-          {
-               afterGoalTimer();
-          }
-          if (!isSynced)
-          {
-               GameObject.Find("Blue Team Score").GetComponent<Text>().text = BScore.ToString();
-               GameObject.Find("Timer").GetComponent<Timer>().timer = (float)PlayerPrefs.GetInt("Time");
-               isSynced = true;
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
 
-          }
-     }
+    void Start()
+    {
+        //Create ref to the object before deactivating
+        BTS = GameObject.Find("BlueTeamScored");
+        BTS.SetActive(false);
+        getScores();
 
-     //Detect when ball entes goal area
-     void OnTriggerExit(Collider obj)
-     {
-          if (obj.gameObject.name == "Ball" && canScore)
-          {
-               Debug.Log("Blue Team Scored!");
-               incrementScore();
-               BTS.SetActive(true);
-               isTimerOn = true;
-          }
-     }
+    }
 
-     //Wait 3 seconds before reloading the scene
-     public float timer = 4;
-     bool isTimerOn = false;
-     void afterGoalTimer()
-     {
-          timer -= Time.deltaTime;
-          if (timer <= 0)
-          {
-               saveScores();
-               saveTime();
-               SceneManager.LoadScene("MiniGame");
-          }
-     }
+    bool canScore = true;
+    void incrementScore()
+    {
+        float vol = UnityEngine.Random.Range(volLowRange, volHighRange);
+        source.PlayOneShot(scoreSound, vol);
 
-     //Load scores
-     void getScores()
-     {
-          //Check if we already have the value in playerprefs
-          if (!PlayerPrefs.HasKey("BlueScore")){
-               BScore = 0;
-               PlayerPrefs.SetInt("BlueScore", 0);
-          }
-          else
-          {
-               BScore = PlayerPrefs.GetInt("BlueScore");
-          }
-               
+        GameObject BScoreOb = GameObject.Find("Blue Team Score");
 
-     }
-     //Used for continuing the scores between goals
-     void saveScores()
-     {
-         PlayerPrefs.SetInt("BlueScore", BScore);
-     }
+        BScore++;
+        BScoreOb.GetComponent<Text>().text = BScore.ToString();
+        canScore = false;
+        saveScores();
+    }
 
-     //Saves the current time on the clock
-     void saveTime()
-     {
-          string currTime = GameObject.Find("Timer").GetComponent<Text>().text.ToString();
-          string[] times = currTime.Split(':');
-          int timeRemaining = (Int32.Parse(times[0]) * 60) + Int32.Parse(times[1]);
-          PlayerPrefs.SetInt("Time", timeRemaining);
-     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTimerOn)
+        {
+            afterGoalTimer();
+        }
+        if (!isSynced)
+        {
+            GameObject.Find("Blue Team Score").GetComponent<Text>().text = BScore.ToString();
+            GameObject.Find("Timer").GetComponent<Timer>().timer = (float)PlayerPrefs.GetInt("Time");
+            isSynced = true;
+
+        }
+    }
+
+    //Detect when ball entes goal area
+    void OnTriggerExit(Collider obj)
+    {
+        if (obj.gameObject.name == "Ball" && canScore)
+        {
+            Debug.Log("Blue Team Scored!");
+            incrementScore();
+            BTS.SetActive(true);
+            isTimerOn = true;
+        }
+    }
+
+    //Wait 3 seconds before reloading the scene
+    public float timer = 4;
+    bool isTimerOn = false;
+    void afterGoalTimer()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            saveScores();
+            saveTime();
+            SceneManager.LoadScene("MiniGame");
+        }
+    }
+
+    //Load scores
+    void getScores()
+    {
+        //Check if we already have the value in playerprefs
+        if (!PlayerPrefs.HasKey("BlueScore"))
+        {
+            BScore = 0;
+            PlayerPrefs.SetInt("BlueScore", 0);
+        }
+        else
+        {
+            BScore = PlayerPrefs.GetInt("BlueScore");
+        }
+
+
+    }
+    //Used for continuing the scores between goals
+    void saveScores()
+    {
+        PlayerPrefs.SetInt("BlueScore", BScore);
+    }
+
+    //Saves the current time on the clock
+    void saveTime()
+    {
+        string currTime = GameObject.Find("Timer").GetComponent<Text>().text.ToString();
+        string[] times = currTime.Split(':');
+        int timeRemaining = (Int32.Parse(times[0]) * 60) + Int32.Parse(times[1]);
+        PlayerPrefs.SetInt("Time", timeRemaining);
+    }
 }
 
 
