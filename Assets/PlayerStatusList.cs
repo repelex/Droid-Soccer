a7488fc8add971752bd50a6fbee5fe1e5f5e8ae6
ajmccutc <mccutcheon.andrew.j@gmail.com>
2016-04-03 15:21:10 -0400

    Player Status
    Now displays player number

--- Assets/PlayerStatusList.cs
+++ Assets/PlayerStatusList.cs
@@ -1,22 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+[RequireComponent(typeof(PhotonView))]
 
-public class PlayerStatusList : MonoBehaviour {
+public class PlayerStatusList : Photon.MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        
+
         GameObject.Find("Player Status").GetComponent<Text>().text = "";
         foreach (PhotonPlayer player in PhotonNetwork.playerList)
         {
-
+            
             Debug.Log(PhotonNetwork.playerList.Length);
-            GameObject.Find("Player Status").GetComponent<Text>().text += player.name+" status: " + player.customProperties["ready"] + "\n";
+            GameObject.Find("Player Status").GetComponent<Text>().text += "player "+player.ID+" status: " + player.customProperties["ready"] + "\n";
         }
 
     }
